a61b7b0532da2fca9126406baa5d0f860c9cbf92
Jacob Leites <jleites356@gmail.com>
2025-10-09 19:57:31 -0400

    Change in Asteroid Script

--- Assets/BigAsteroidSeperate.cs
+++ Assets/BigAsteroidSeperate.cs
@@ -10,30 +10,34 @@ public class BigAsteroidSeperate : MonoBehaviour
  
     }
     // Upon click, turn big asteroid into two
-    private void OnMouseDown(){
-        // Store original properties
-        Vector3 originalPosition = transform.position;
-        Vector3 originalScale = transform.localScale;      
+    private void OnCollisionEnter(Collision collision){
 
-        // Create new game object
-        GameObject newBlock = Instantiate(gameObject, originalPosition, transform.rotation = Quaternion.identity);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Store original properties
+            Vector3 originalPosition = transform.position;
+            Vector3 originalScale = transform.localScale;
 
-        // Halve scale of asteroid on the x-axis
-        // Split x-axis in half
-        Vector3 splitScale = new Vector3(originalScale.x / 2f, originalScale.y / 2f, originalScale.z / 2f);
-        
-        // Turn the normal scale into 'splitScale'
-        transform.localScale = splitScale;
-        newBlock.transform.localScale = splitScale;
+            // Create new game object
+            GameObject newBlock = Instantiate(gameObject, originalPosition, transform.rotation = Quaternion.identity);
 
-        float offset = (originalScale.x / 4f) + 5;
-        transform.position -= transform.right * offset;
-        newBlock.transform.position += transform.right * offset;
+            // Halve scale of asteroid on the x-axis
+            // Split x-axis in half
+            Vector3 splitScale = new Vector3(originalScale.x / 2f, originalScale.y / 2f, originalScale.z / 2f);
+
+            // Turn the normal scale into 'splitScale'
+            transform.localScale = splitScale;
+            newBlock.transform.localScale = splitScale;
+
+            float offset = (originalScale.x / 4f) + 5;
+            transform.position -= transform.right * offset;
+            newBlock.transform.position += transform.right * offset;
 
 
-        Destroy(newBlock.GetComponent<BigAsteroidSeperate>()); // Destroy script on the clone
-        Destroy(this); // Destroy script on the original
-    }
+            Destroy(newBlock.GetComponent<BigAsteroidSeperate>()); // Destroy script on the clone
+            Destroy(this); // Destroy script on the original
+            }
+        }
     // Update is called once per frame
     void Update()
     {
